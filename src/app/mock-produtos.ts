f99d69673b22f2c0fa74c28f69260543695bfd72
amanda <amanda.piva01@fatec.sp.gov.br>
2022-11-02 13:44:34 -0300

    Adicionando mock com 10 produtos

---
+++ src/app/mock-produtos.ts
@@ -0,0 +1,14 @@
+import {InterProd} from './produto';
+
+export const PROD: InterProd[] = [
+    {id: 12, name: "Geladeira", type: "Eletrodomésticos", preco: 2500.00, marca: "Brastemp"},
+    {id: 13, name: "Fogão quatro bocas", type: "Eletrodomésticos", preco: 1500.00, marca: "Esmaltec"},
+    {id: 14, name: "Armário de cozinha", type: "Móveis", preco: 720.00, marca: "Colormaq"},
+    {id: 15, name: "Sofá", type: "Móveis", preco: 999.90, marca: "Etina"},
+    {id: 16, name: "TV Smart", type: "Eletrônicos", preco: 3500.00, marca: "Sansung"},
+    {id: 17, name: "Notebook Intel Core i7", type: "Eletrônicos", preco: 6000.00, marca: "Dell"},
+    {id: 18, name: "Máquina de Lavar", type: "Eletrodomésticos", preco: 1800.00, marca: "LG"},
+    {id: 19, name: "Iphone", type: "Eletrônicos", preco: 15000.00, marca: "Apple"},
+    {id: 20, name: "Celular Android", type: "Eletrônicos", preco: 4000.00, marca: "Sansung"},
+    {id: 21, name: "Geladeira", type: "Eletrodomésticos", preco: 3200.00, marca: "Brastemp"}
+];
